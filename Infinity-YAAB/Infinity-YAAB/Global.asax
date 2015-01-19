b87929eaddf480508df4bb4f5bff420fa94b34c0
tcastile <tracy.castile@gmail.com>
2015-01-18 16:14:33 -0800

    Initial project creation - basic mvc template

---
+++ Infinity-YAAB/Infinity-YAAB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Infinity_YAAB.MvcApplication" Language="C#" %>
---
+++ Infinity-YAAB/Infinity-YAAB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Infinity_YAAB.Startup))]
+namespace Infinity_YAAB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
