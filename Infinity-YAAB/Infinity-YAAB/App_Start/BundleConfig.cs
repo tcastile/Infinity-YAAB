205c194face86ebe44f89b5be40a86428f6f1f53
monizjason <moniz.jason@gmail.com>
2015-01-19 19:52:50 -0800

    Added jquery ui, index body content additions, css changes

--- Infinity-YAAB/Infinity-YAAB/App_Start/BundleConfig.cs
+++ Infinity-YAAB/Infinity-YAAB/App_Start/BundleConfig.cs
@@ -25,11 +25,16 @@ namespace Infinity_YAAB
             bundles.Add(new ScriptBundle("~/ScriptBundles/jquery-core").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/ScriptBundles/jquery-core").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/ScriptBundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/ScriptBundles/bootstrap-core").Include(
                       "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/ScriptBundles/InfinityFrontEnd").Include("~/Scripts/layout.js"));
 
             
         }
